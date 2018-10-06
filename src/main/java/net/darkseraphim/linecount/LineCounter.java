9b2e400fc97198f0563de9817cc1a33616910b13
Bo0tzz <btdewilligen@gmail.com>
2018-10-06 13:48:58 +0200

    Implement and use the FilePathSupplier

--- src/main/java/net/darkseraphim/linecount/LineCounter.java
+++ src/main/java/net/darkseraphim/linecount/LineCounter.java
@@ -1,26 +1,27 @@
 package net.darkseraphim.linecount;
 
-import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Files;
-import java.util.stream.StreamSupport;
 
 public class LineCounter {
   public static void main(String[] args) {
     if (args.length != 1) {
       panic("Expected single file argument");
     }
-    File file = new File(args[0]);
-    if (!file.exists() || !file.isFile()) {
-      panic("File does not exist ◕_◕");
+
+    FilePathSupplier filePathSupplier;
+    try {
+      filePathSupplier = new DefaultFilePathSupplier().with(args[0]);
+    } catch (LineCountException e) {
+      panic(e.getMessage());
+      return;
     }
 
     LineCountStrategy strategy = new DefaultLineCountStrategy();
     try {
-      BigInteger count = strategy.countLines(file.toPath());
+      BigInteger count = strategy.countLines(filePathSupplier.get());
       System.out.println(count);
-    } catch (IOException ex) {
+    } catch (IOException | LineCountException ex) {
       panic(ex.getMessage());
     }
   }
